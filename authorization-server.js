30cd733b586185d03e9608e52e3d57f0cdb2c7e7
Artur Zinurov <38508285+ArturZinurov@users.noreply.github.com>
2022-06-17 12:09:16 -0500

    fix 1

--- authorization-server.js
+++ authorization-server.js
@@ -78,7 +78,7 @@ app.get("/authorize", (req, res) => {
 
 app.post("/approve", (req, res) => {
 	const {userName, password, requestId} = req.body
-	if (userName || users[userName] !== password) {
+	if (!userName || users[userName] !== password) {
 		res.status(401).send("Error: user not authorized")
 		return
 	}
